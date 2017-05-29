8fcfaa022fa26836bdf509ccd7f4ddc91195b548
Sami Oksanen <sami.oksanen95@gmail.com>
2017-05-29 15:23:56 +0300

    error handling for input sections

--- hangman.py
+++ hangman.py
@@ -3,24 +3,29 @@ from word_list import words
 from random import randrange
 
 def begin():
-    play = input('Do you want to play a round of hangman guessing game (yes/no): \n')
-    if play == 'yes':
-        choose = ''
-        word = ''
-        print('Themes: ')
-        for key in sorted(words.keys()):
-            print(key)
-        while word == '':
-            choose = input('Please choose a theme: \n').lower()
-            if words[choose]:
-                x = randrange(len(words[choose]))
-                word = words[choose][x].upper()
-                game(word)
-            else:
-                print("We don't have {} theme".format(choose))
-
-    elif play == 'no':
-        print('Okey then. Good bye.')
+    while True:
+        play = input('Do you want to play a round of hangman guessing game (yes/no): \n')
+        if play == 'yes' or play == 'YES' or play == 'y':
+            choose = ''
+            word = ''
+            print('Themes: ')
+            for key in sorted(words.keys()):
+                print(key)
+            while True:
+                choose = input('Please choose a theme: \n').lower()
+                if choose in words:
+                    x = randrange(len(words[choose]))
+                    word = words[choose][x].upper()
+                    game(word)
+                    break
+                else:
+                    print("We don't have {} theme".format(choose))
+            break
+        elif play == 'no' or play == 'NO' or play == 'n':
+            print('Okey then. Good bye.')
+            break
+        else:
+            print('Please answer correctly')
 
 def game(word):
     f = 0
@@ -34,7 +39,14 @@ def game(word):
     while f < 9:
         print(str(fill_word.count("_")) + " empty letters")
         print("Word: " + fill_word)
-        letter = input('Please give a letter: \n').upper()
+        while True:
+            letter = input('Please give a letter: \n').upper()
+            if 1 <len(letter) > 1:
+                print('Please give only one letter')
+            elif letter not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ -.,':
+                print('Please give one of these letters: ABCDEFGHIJKLMNOPQRSTUVWXYZ -.,')
+            else:
+                break
         correct = 0
         for i in range(len(word)):
             if letter == word[i]:
