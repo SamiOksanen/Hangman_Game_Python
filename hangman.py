9c42b6243d68c85c6c128092d84a6b160d13e2e8
Sami Oksanen <sami.oksanen95@gmail.com>
2017-05-29 12:01:27 +0300

    word themes for the game and the game works generally better and more logically

--- hangman.py
+++ hangman.py
@@ -1,26 +1,45 @@
 from failure import failed
-
-word = 'asd'.upper()
+from word_list import words
+from random import randrange
 
 def begin():
-    play = input('Do you want to play a round of hangman guessing game (yes/no): ')
+    play = input('Do you want to play a round of hangman guessing game (yes/no): \n')
     if play == 'yes':
-        game()
+        choose = ''
+        word = ''
+        print('Themes: ')
+        for key in sorted(words.keys()):
+            print(key)
+        while word == '':
+            choose = input('Please choose a theme: \n').lower()
+            if words[choose]:
+                x = randrange(len(words[choose]))
+                word = words[choose][x].upper()
+                game(word)
+            else:
+                print("We don't have {} theme".format(choose))
+
     elif play == 'no':
         print('Okey then. Good bye.')
 
-def game():
+def game(word):
     f = 0
     fill_word = '_' * len(word)
+    for i in range(len(word)):
+        if ' ' == word[i]:
+            characters = list(fill_word)
+            characters[i] = ' '
+            fill_word = ''.join(characters)
     print("Let's begin then!")
     while f < 9:
+        print(str(fill_word.count("_")) + " empty letters")
         print("Word: " + fill_word)
-        letter = input('Please give a letter: ').upper()
+        letter = input('Please give a letter: \n').upper()
         correct = 0
-        for char in word:
-            if char == letter:
+        for i in range(len(word)):
+            if letter == word[i]:
                 characters = list(fill_word)
-                characters[word.index(char)] = letter
+                characters[i] = letter
                 fill_word = ''.join(characters)
                 correct += 1
         if correct == 0:
@@ -30,6 +49,7 @@ def game():
                 begin()
                 break
         if fill_word == word:
+            print("Word: " + fill_word)
             print('!!!YOU WIN, CONGRATULATIONS!!!')
             begin()
             break
---
+++ word_list.py
@@ -0,0 +1,30 @@
+words = {
+    "superheroes": [
+        "Spiderman",
+        "Iron Man",
+        "Hulk",
+        "Batman",
+        "Superman",
+        "Arrow",
+        "Flash",
+        "Daredevil",
+        "Iron Fist"
+    ],
+    "fruits": [
+        "pineapple",
+        "lime",
+        "pomegranate"
+        "grape",
+        "dragon fruit",
+        "watermelon",
+        "durian"
+    ],
+    "capitals": [
+        "Helsinki",
+        "Stockholm",
+        "Berlin",
+        "Warsaw",
+        "Beijing",
+        "Reykjavik"
+    ]
+}
